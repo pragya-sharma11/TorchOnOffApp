eb0587fa696134c07a7afee0742c14ee3ceadd47
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-06 21:26:36 +0530

    make functionality for button 3.

--- app/src/main/java/com/example/torchonoffapp/MainActivity.java
+++ app/src/main/java/com/example/torchonoffapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.torchonoffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1, b2;
+    Button b1, b2, b3;
     CameraManager cm;
     private  boolean flash =false;
     @Override
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         b1 = findViewById(R.id.button1);
         b2 = findViewById(R.id.button2);
+        b3 = findViewById(R.id.button3);
         cm = (CameraManager) getSystemService(CAMERA_SERVICE);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 flashOff();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+                finish();
             }
         });
     }
