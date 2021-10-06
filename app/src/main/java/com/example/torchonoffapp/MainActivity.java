9bf00d519e9830c5a8e148542b922acdb18ed391
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-06 21:00:13 +0530

    get elements by id

--- app/src/main/java/com/example/torchonoffapp/MainActivity.java
+++ app/src/main/java/com/example/torchonoffapp/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.torchonoffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1, b2;
+    CameraManager cm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button1);
+        b2 = findViewById(R.id.button2);
+        cm = (CameraManager) getSystemService(CAMERA_SERVICE);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
