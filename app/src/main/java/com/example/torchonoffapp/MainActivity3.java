e27bc9791ba8e4bbbbebfe32ccf243a3c7ea71f2
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-06 21:25:17 +0530

    add a button to go to second activity.

--- app/src/main/java/com/example/torchonoffapp/MainActivity3.java
+++ app/src/main/java/com/example/torchonoffapp/MainActivity3.java
@@ -2,13 +2,52 @@ package com.example.torchonoffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    ToggleButton tb;
+    CameraManager cm;
+    private  boolean flash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        tb = findViewById(R.id.toggleButton);
+        cm = (CameraManager) getSystemService(CAMERA_SERVICE);
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(!isChecked){
+                    flashOn();
+                }
+                else {
+                    flashOff();
+                }
+            }
+        });
+    }
+    public void flashOn(){
+        try {
+            String id  = cm.getCameraIdList()[0];
+            cm.setTorchMode(id,true);
+            flash=true;
+
+        }catch (CameraAccessException e){
+
+        }
+    }
+    public void flashOff(){
+        try {
+            String id  = cm.getCameraIdList()[0];
+            cm.setTorchMode(id,false);
+            flash=false;
+
+        }catch (CameraAccessException e){
+
+        }
     }
 }
