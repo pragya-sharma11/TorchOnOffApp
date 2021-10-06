ca4a7da9b20b3f56480c045ea5c793f0e1a98d52
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-06 21:19:04 +0530

    make functionality for image button.

--- app/src/main/java/com/example/torchonoffapp/MainActivity2.java
+++ app/src/main/java/com/example/torchonoffapp/MainActivity2.java
@@ -2,13 +2,50 @@ package com.example.torchonoffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    ImageButton i;
+    CameraManager cm;
+    private boolean flash = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        i = findViewById(R.id.imageButton);
+        cm = (CameraManager) getSystemService(CAMERA_SERVICE);
+        i.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(flash==false){
+                    flashOn();
+                }
+                else{
+                    flashOff();
+                }
+            }
+        });
+    }
+    public void flashOn(){
+        try {
+            String id  = cm.getCameraIdList()[0];
+            cm.setTorchMode(id,true);
+            flash=true;
+        }catch (CameraAccessException e){
+
+        }
+    }
+    public void flashOff(){
+        try {
+            String id  = cm.getCameraIdList()[0];
+            cm.setTorchMode(id,false);
+            flash=false;
+        }catch (CameraAccessException e){
+
+        }
     }
 }
